d5a4ab359692d57503280d31ffbff8e35eae0fd5
unilock <unilock@fennet.rentals>
2021-10-24 20:47:57 -0400

    sigh
    Signed-off-by: unilock <unilock@fennet.rentals>

--- scripts/defaults-gnome-write.sh
+++ scripts/defaults-gnome-write.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 source functions/common
 
-print_line "Setting up GNOME defaults."
+print_line "Setting GNOME defaults."
 
 print_line " - 'minimize-or-previews' click-action for dash-to-dock"
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize-or-previews'
