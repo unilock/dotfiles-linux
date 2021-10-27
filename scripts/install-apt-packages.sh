522bf5ce9539ea06d446a80661ffdb6729834eb5
unilock <unilock@fennet.rentals>
2021-10-26 22:41:41 -0400

    make print_line (much) more readable
    Signed-off-by: unilock <unilock@fennet.rentals>

--- scripts/install-apt-packages.sh
+++ scripts/install-apt-packages.sh
@@ -3,14 +3,14 @@
 
 # Install long list of packages.
 
-print_line "Updating apt packages."
+print_line "Updating apt cache."
 sudo apt update
 
-print_line "Upgrading apt packages."
+print_line "Upgrading packages."
 sudo apt upgrade -y
 
 print_line "Purging unnecessary packages."
 sudo apt autoremove -y --purge
 
-print_line "Installing apt packages."
+print_line "Installing packages."
 xargs sudo apt install -y < lists/apt-packages
