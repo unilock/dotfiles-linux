abe1c9f0cc557dff78ad2c82c0f9999dd5b344b9
unilock <unilock@fennet.rentals>
2021-11-26 18:56:30 -0500

    Fix Microsoft Edge It messes with /etc/apt, so we set it up the way it wants
    Signed-off-by: unilock <unilock@fennet.rentals>

--- scripts/install-special-packages.sh
+++ scripts/install-special-packages.sh
@@ -13,9 +13,6 @@ print_line "Setting up special packages."
 print_line "Adding AdoptOpenJDK repository."
 add_apt_repo "adoptopenjdk.gpg" "adoptopenjdk.list" "https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public" "https://adoptopenjdk.jfrog.io/adoptopenjdk/deb $_codename main"
 
-print_line "Adding Microsoft Edge repository."
-add_apt_repo "microsoft.gpg" "microsoft-edge.list" "https://packages.microsoft.com/keys/microsoft.asc" "[arch=amd64] https://packages.microsoft.com/repos/edge stable main"
-
 print_line "Adding Sublime Text repository."
 add_apt_repo "sublimehq.gpg" "sublimehq.list" "https://download.sublimetext.com/sublimehq-pub.gpg" "https://download.sublimetext.com/ apt/stable/"
 
@@ -24,6 +21,10 @@ add_apt_repo "nodesource.gpg" "nodesource.list" "https://deb.nodesource.com/gpgk
 
 print_line "Adding WineHQ repository."
 add_apt_repo "winehq.gpg" "winehq.list" "https://dl.winehq.org/wine-builds/winehq.key" "https://dl.winehq.org/wine-builds/ubuntu/ $_codename main"
+
+print_line "Adding Microsoft Edge repository."
+curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-beta.list'
 
 print_line "Updating apt cache (again)."
 sudo apt update
