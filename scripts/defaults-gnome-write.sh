d023c913deb33c03b7acec96baaa86613a4c0411
unilock <unilock@fennet.rentals>
2021-11-26 18:56:56 -0500

    Flameshot
    Signed-off-by: unilock <unilock@fennet.rentals>

--- scripts/defaults-gnome-write.sh
+++ scripts/defaults-gnome-write.sh
@@ -5,3 +5,19 @@ print_line "Setting up GNOME defaults."
 
 print_line " - 'minimize-or-previews' click-action for dash-to-dock"
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize-or-previews'
+
+# --- Flameshot ---
+
+print_line " - Bind Flameshot to PRINT SCREEN"
+
+# Disable GNOME's default Print Screen binding
+gsettings set org.gnome.settings-daemon.plugins.media-keys screenshot "'[]'"
+
+# Edit this line if wanting to add more custom keybinds (e.g. "['.../custom0/','.../custom1/','.../custom2/']" etc.)
+# Then add more "gsettings set ...media-keys.custom-keybinding:/..." lines appropriately
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+
+# Name, Command, Binding (scroll to the right)                                                                  Edit this number for other keybinds ↴
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "'Flameshot'"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "'flatpak run org.flameshot.Flameshot gui'"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "'Print'"
