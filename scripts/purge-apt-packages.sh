5d022b398667325db02e2697b9f21629be012035
unilock <unilock@fennet.rentals>
2022-01-18 19:38:09 -0500

    purge gedit (we use Sublime Text)
    Signed-off-by: unilock <unilock@fennet.rentals>

--- scripts/purge-apt-packages.sh
+++ scripts/purge-apt-packages.sh
@@ -6,5 +6,8 @@
 print_line "Purging Firefox."
 sudo apt purge -y firefox*
 
+print_line "Purging gedit."
+sudo apt purge -y gedit*
+
 print_line "Purging LibreOffice (we use the flatpak)."
 sudo apt purge -y libreoffice*
