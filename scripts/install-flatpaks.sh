8d8d976b636ec13fbfcd0d7c9c5a23b50947ad27
unilock <unilock@fennet.rentals>
2022-03-12 11:04:58 -0500

    flatpaks: add beta repo; GoldenDict + Steam Beta (+ Proton)
    Signed-off-by: unilock <unilock@fennet.rentals>

--- scripts/install-flatpaks.sh
+++ scripts/install-flatpaks.sh
@@ -3,11 +3,15 @@
 
 # Install long list of flatpaks.
 
-print_line "Updating flatpak cache."
-flatpak update
+print_line "Adding flathub-beta repository."
+flatpak remote-add --user flathub-beta "https://flathub.org/beta-repo/flathub-beta.flatpakrepo"
+
+print_line "Updating flatpak appstream."
+flatpak update --appstream
 
 print_line "Uninstalling unnecessary flatpaks."
 flatpak uninstall -y --unused
 
 print_line "Installing flatpaks."
 xargs flatpak install -y flathub < lists/flatpaks
+xargs flatpak install -y flathub-beta < lists/flatpaks-beta
